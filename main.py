45f3a6a3c4f07b2fb1de46d64f547452b8449375
Nick Zhukov <njhuckov@gmail.com>
2020-10-06 10:45:11 +0300

    hello world template

---
+++ main.py
@@ -0,0 +1,23 @@
+import tornado.ioloop
+import tornado.web
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello, world")
+
+        # localhost:8888/ -> Hello, world
+
+def make_app():
+    return tornado.web.Application([
+        (r"/", MainHandler), 
+    ])
+
+    # 
+
+if __name__ == "__main__":
+#python3 main.py or python main.py
+	
+
+    app = make_app()
+    app.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
