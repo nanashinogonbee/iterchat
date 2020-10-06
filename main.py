0b16a37f8fd20d68ec562e52ca411796883164d8
Nick Zhukov <njhuckov@gmail.com>
2020-10-06 14:30:13 +0300

    echo websocket message

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tornado.ioloop
 import tornado.web
+import tornado.websocket
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
@@ -7,12 +8,32 @@ class MainHandler(tornado.web.RequestHandler):
 
         # localhost:8888/ -> Hello, world
 
+class IndexHandler(tornado.web.RequestHandler):
+	# отображать какую-то индексную страницу
+	def get(self):
+		self.render("index.html")
+
+
+class EchoWebSocketHandler(tornado.websocket.WebSocketHandler):
+	
+	def open(self):
+		print('websocket is opened')
+
+	def on_message(self, message):
+		self.write_message(f" Message was sent: {message} ")
+
+	def on_close(self):
+		print('websocket is closed')
+
+
+
 def make_app():
     return tornado.web.Application([
-        (r"/", MainHandler), 
+        (r"/", MainHandler),
+        (r"/index", IndexHandler),
+        (r"/websocket", EchoWebSocketHandler), 
     ])
 
-    # 
 
 if __name__ == "__main__":
 #python3 main.py or python main.py
