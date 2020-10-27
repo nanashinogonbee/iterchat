0039af73337c86f97186291bf97ac9d00af61925
Nick Zhukov <njhuckov@gmail.com>
2020-10-27 11:32:07 +0300

    message buffer added

--- main.py
+++ main.py
@@ -3,10 +3,32 @@ import tornado.web
 import tornado.websocket
 
 
+class MessagesBuffer():
+
+	def __init__(self, messages):
+
+		self.__messages = []
+		for m in messages:
+			self.__messages.append(m)
+
+
+	def add_message(self, message):
+		self.__messages.append(message)
+
+
+	def get_messages(self):
+		return self.__messages
+
+
+
+globalmessagebuffer = MessagesBuffer([ {"message":"The chat was started."}, {"message": "Say hello to everybody!"} ])
+
+
 class IndexHandler(tornado.web.RequestHandler):
 	# отображать какую-то индексную страницу
 	def get(self):
-		self.render("index.html")
+		self.render("index.html", messages = globalmessagebuffer.get_messages())
+
 
 
 class EchoWebSocketHandler(tornado.websocket.WebSocketHandler):
@@ -17,11 +39,12 @@ class EchoWebSocketHandler(tornado.websocket.WebSocketHandler):
 		print('websocket is opened')
 
 	def on_message(self, message):
+		# import time
+		# key = time.strftime("%Y%m%d%H%M%S")
+		new_message = {'message': message}
+		
+		globalmessagebuffer.add_message(new_message)
 
-		import time
-		key = time.strftime("%Y%m%d%H%M%S")
-		EchoWebSocketHandler.messages[key] = message
-		print(EchoWebSocketHandler.messages)
 		self.write_message(f" Message was sent: {message} ")
 
 	def on_close(self):
